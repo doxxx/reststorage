d823aa496eedd70cd4962b075c9ebb711158c951
Gordon Tyler <gordon@doxxx.net>
2015-05-04 02:43:57 -0400

    Fix docker build command for reststorage app.

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd `dirname $0`
-sudo docker build -t localrest .
+sudo docker build -t reststorage .
 sudo docker rm -f localrest
 sudo docker run -d -p 8080:8080 --name localrest --link localredis:redis reststorage
